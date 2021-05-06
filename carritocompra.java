637d12fcaaf0ea03b0ff1da8a1768be9d7e497cf
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2021-05-06 11:04:31 +0200

    sadfasdf

---
+++ carritocompra.java
@@ -0,0 +1,362 @@
+/*
+ * 		file.Classes[0].Name	"CarritoDeLaCompra"	string
+-		file.Operands	Count = 229	System.Collections.Generic.List<string>
+		[0]	"package"	string
+		[1]	"com"	string
+		[2]	"."	string
+		[3]	"arquitecturajava"	string
+		[4]	";"	string
+		[5]	"import"	string
+		[6]	"java"	string
+		[7]	"."	string
+		[8]	"util"	string
+		[9]	"."	string
+		[10]	"Collection"	string
+		[11]	";"	string
+		[12]	"public"	string
+		[13]	"class"	string
+		[14]	"CarritoDeLaCompra"	string
+		[15]	"{"	string
+		[16]	"private"	string
+		[17]	"Collection"	string
+		[18]	"Integer"	string
+		[19]	"precios"	string
+		[20]	";"	string
+		[21]	"public"	string
+		[22]	"CarritoDeLaCompra"	string
+		[23]	"("	string
+		[24]	"Collection"	string
+		[25]	"Integer"	string
+		[26]	"precios"	string
+		[27]	")"	string
+		[28]	"{"	string
+		[29]	"this"	string
+		[30]	"."	string
+		[31]	"precios"	string
+		[32]	"precios"	string
+		[33]	";"	string
+		[34]	"}"	string
+		[35]	"public"	string
+		[36]	"int"	string
+		[37]	"calcularPrecioTotal"	string
+		[38]	"("	string
+		[39]	")"	string
+		[40]	"{"	string
+		[41]	"int"	string
+		[42]	"precioTotal"	string
+		[43]	"0"	string
+		[44]	";"	string
+		[45]	"("	string
+		[46]	"Integer"	string
+		[47]	"precio"	string
+		[48]	":"	string
+		[49]	"precios"	string
+		[50]	")"	string
+		[51]	"{"	string
+		[52]	"precioTotal"	string
+		[53]	"precio"	string
+		[54]	";"	string
+		[55]	"}"	string
+		[56]	"return"	string
+		[57]	"precioTotal"	string
+		[58]	";"	string
+		[59]	"}"	string
+		[60]	"public"	string
+		[61]	"int"	string
+		[62]	"contarNumeroProductos"	string
+		[63]	"("	string
+		[64]	")"	string
+		[65]	"{"	string
+		[66]	"return"	string
+		[67]	"precios"	string
+		[68]	"."	string
+		[69]	"size"	string
+		[70]	"("	string
+		[71]	")"	string
+		[72]	";"	string
+		[73]	"}"	string
+		[74]	"public"	string
+		[75]	"int"	string
+		[76]	"calcularPrecioTotalLambda"	string
+		[77]	"("	string
+		[78]	")"	string
+		[79]	"{"	string
+		[80]	"int"	string
+		[81]	"precioTotal"	string
+		[82]	"this"	string
+		[83]	"."	string
+		[84]	"precios"	string
+		[85]	"."	string
+		[86]	"stream"	string
+		[87]	"("	string
+		[88]	")"	string
+		[89]	"."	string
+		[90]	"mapToInt"	string
+		[91]	"("	string
+		[92]	"Integer"	string
+		[93]	"::"	string
+		[94]	"intValue"	string
+		[95]	")"	string
+		[96]	"."	string
+		[97]	"sum"	string
+		[98]	"("	string
+		[99]	")"	string
+		[100]	";"	string
+		[101]	"return"	string
+		[102]	"precioTotal"	string
+		[103]	";"	string
+		[104]	"}"	string
+		[105]	"public"	string
+		[106]	"long"	string
+		[107]	"calcularDescuentoTotal"	string
+		[108]	"("	string
+		[109]	"int"	string
+		[110]	"cantidadConDescuento"	string
+		[111]	")"	string
+		[112]	"{"	string
+		[113]	"long"	string
+		[114]	"descuentoTotal"	string
+		[115]	"0"	string
+		[116]	";"	string
+		[117]	"("	string
+		[118]	"Integer"	string
+		[119]	"precio"	string
+		[120]	":"	string
+		[121]	"precios"	string
+		[122]	")"	string
+		[123]	"{"	string
+		[124]	"("	string
+		[125]	"precio"	string
+		[126]	"cantidadConDescuento"	string
+		[127]	")"	string
+		[128]	"{"	string
+		[129]	"descuentoTotal"	string
+		[130]	"("	string
+		[131]	"cantidadConDescuento"	string
+		[132]	"5"	string
+		[133]	")"	string
+		[134]	"100"	string
+		[135]	";"	string
+		[136]	"}"	string
+		[137]	"}"	string
+		[138]	"return"	string
+		[139]	"descuentoTotal"	string
+		[140]	";"	string
+		[141]	"}"	string
+		[142]	"public"	string
+		[143]	"long"	string
+		[144]	"calcularDescuentoTotalLambda"	string
+		[145]	"("	string
+		[146]	"int"	string
+		[147]	"cantidadConDescuento"	string
+		[148]	")"	string
+		[149]	"{"	string
+		[150]	"long"	string
+		[151]	"descuentoTotal"	string
+		[152]	"0"	string
+		[153]	";"	string
+		[154]	"Long"	string
+		[155]	"numeroDeDescuentos"	string
+		[156]	"this"	string
+		[157]	"."	string
+		[158]	"precios"	string
+		[159]	"."	string
+		[160]	"stream"	string
+		[161]	"("	string
+		[162]	")"	string
+		[163]	"."	string
+		[164]	"filter"	string
+		[165]	"("	string
+		[166]	"precio"	string
+		[167]	"->"	string
+		[168]	"precio"	string
+		[169]	"."	string
+		[170]	"intValue"	string
+		[171]	"("	string
+		[172]	")"	string
+		[173]	"cantidadConDescuento"	string
+		[174]	")"	string
+		[175]	"."	string
+		[176]	"count"	string
+		[177]	"("	string
+		[178]	")"	string
+		[179]	";"	string
+		[180]	"descuentoTotal"	string
+		[181]	"("	string
+		[182]	"cantidadConDescuento"	string
+		[183]	"5"	string
+		[184]	"100"	string
+		[185]	")"	string
+		[186]	"numeroDeDescuentos"	string
+		[187]	";"	string
+		[188]	"return"	string
+		[189]	"descuentoTotal"	string
+		[190]	";"	string
+		[191]	"}"	string
+		[192]	"public"	string
+		[193]	"boolean"	string
+		[194]	"detectarErrorFindAny"	string
+		[195]	"("	string
+		[196]	")"	string
+		[197]	"{"	string
+		[198]	"return"	string
+		[199]	"this"	string
+		[200]	"."	string
+		[201]	"precios"	string
+		[202]	"."	string
+		[203]	"stream"	string
+		[204]	"("	string
+		[205]	")"	string
+		[206]	"."	string
+		[207]	"filter"	string
+		[208]	"("	string
+		[209]	"precio"	string
+		[210]	"->"	string
+		[211]	"precio"	string
+		[212]	"."	string
+		[213]	"intValue"	string
+		[214]	"("	string
+		[215]	")"	string
+		[216]	"0"	string
+		[217]	")"	string
+		[218]	"."	string
+		[219]	"findAny"	string
+		[220]	"("	string
+		[221]	")"	string
+		[222]	"."	string
+		[223]	"isPresent"	string
+		[224]	"("	string
+		[225]	")"	string
+		[226]	";"	string
+		[227]	"}"	string
+		[228]	"}"	string
++		Raw View		
+-		file.Operators	Count = 24	System.Collections.Generic.List<string>
+		[0]	"<"	string
+		[1]	">"	string
+		[2]	"<"	string
+		[3]	">"	string
+		[4]	"="	string
+		[5]	"="	string
+		[6]	"for"	string
+		[7]	"+="	string
+		[8]	"="	string
+		[9]	"="	string
+		[10]	"for"	string
+		[11]	"if"	string
+		[12]	">="	string
+		[13]	"+="	string
+		[14]	"*"	string
+		[15]	"/"	string
+		[16]	"="	string
+		[17]	"="	string
+		[18]	">="	string
+		[19]	"="	string
+		[20]	"*"	string
+		[21]	"/"	string
+		[22]	"*"	string
+		[23]	"<"	string
++		Raw View		
+		file.Node.EntityId	1270026321	long
+
+		BM_CognitiveComplexity	1.6388888888888888	double
+		BM_CouplingBetweenObjectClasses	0	double
+		BM_CyclomaticComplexity	0.6944444444444444	double
+		BM_DepthOfInheritance	0	int
+		BM_EffectiveComplexityScoreLogicalComplexity	0	double
+		BM_EffectiveComplexityScoreRelations	0	double
+		BM_EffectiveComplexityScoreSoftwareScience	0	double
+		BM_HalsteadNumOperands	32.611111111111114	double
+		BM_HalsteadNumOperators	4.138888888888889	double
+		BM_HalsteadNumUniqueOperands	18.5	double
+		BM_HalsteadNumUniqueOperators	3.5555555555555554	double
+		BM_InherentComplexityScoreLogicalComplexity	0	double
+		BM_InherentComplexityScoreRelations	0	double
+		BM_InherentComplexityScoreSoftwareScience	0	double
+		BM_LackOfCohesion	1	double
+		BM_LinesOfCode	39	int
+		BM_LinesOfComments	13	int
+		BM_LinesPseudoDeleted	0	int
+		BM_NestingDepth	2	int
+		BM_Size	1197	long
+		Binary	false	bool
+	
+ */
+package com.arquitecturajava;
+
+import java.util.Collection;
+
+public class CarritoDeLaCompra {
+
+	private Collection<Integer> precios;
+
+	public CarritoDeLaCompra(Collection<Integer> precios) {
+
+		this.precios = precios;
+	}
+
+	public int calcularPrecioTotal() {
+
+		int precioTotal = 0;
+
+		for (Integer precio : precios) {
+
+			precioTotal += precio;
+
+		}
+		return precioTotal;
+	}
+
+	public int contarNumeroProductos() {
+
+		return precios.size();
+	}
+
+	/*
+	 * Como podéis observar, hacemos uso del stream, el cual nos recogerá cada
+	 * objeto de la Collection precios (this.precios.stream()) y lo mapeará a
+	 * Integers (mapToInt()).
+	 */
+	public int calcularPrecioTotalLambda() {
+		int precioTotal = this.precios.stream().mapToInt(Integer::intValue).sum();
+		return precioTotal;
+	}
+
+	public long calcularDescuentoTotal(int cantidadConDescuento) {
+
+		long descuentoTotal = 0;
+
+		for (Integer precio : precios) {
+			if (precio >= cantidadConDescuento) {
+				descuentoTotal += (cantidadConDescuento * 5) / 100;
+			}
+		}
+		return descuentoTotal;
+	}
+
+	/*
+	 * nuevo método que use Stream y Lambda:
+	 */
+	public long calcularDescuentoTotalLambda(int cantidadConDescuento) {
+
+		long descuentoTotal = 0;
+
+		Long numeroDeDescuentos = this.precios.stream().filter(precio -> precio.intValue() >= cantidadConDescuento)
+				.count();
+
+		descuentoTotal = (cantidadConDescuento * 5 / 100) * numeroDeDescuentos;
+
+		return descuentoTotal;
+	}
+
+	/*
+	 * findAny() nos devuelve un Optional cuando se cumple la condición de filter(). 
+	 * Este Optional tiene como método isPresent() el cual si encuentra una coincidencia devolverá true. 
+	 * Hay que aclarar que esta forma recorre todo el stream.
+	 */
+	public boolean detectarErrorFindAny() {
+
+		return this.precios.stream().filter(precio -> precio.intValue() < 0).findAny().isPresent();
+	}
+
+}
---
+++ principal.java
@@ -0,0 +1,244 @@
+/*
+ * 		file.Classes[0].Name	"Principal"	string
+-		file.Operands	Count = 178	System.Collections.Generic.List<string>
+		[0]	"import"	string
+		[1]	"java"	string
+		[2]	"."	string
+		[3]	"util"	string
+		[4]	"."	string
+		[5]	"Map"	string
+		[6]	";"	string
+		[7]	"public"	string
+		[8]	"class"	string
+		[9]	"Principal"	string
+		[10]	"{"	string
+		[11]	"public"	string
+		[12]	"static"	string
+		[13]	"void"	string
+		[14]	"main"	string
+		[15]	"("	string
+		[16]	"String"	string
+		[17]	"["	string
+		[18]	"]"	string
+		[19]	"args"	string
+		[20]	")"	string
+		[21]	"{"	string
+		[22]	"Map"	string
+		[23]	"Integer"	string
+		[24]	","	string
+		[25]	"Integer"	string
+		[26]	"map"	string
+		[27]	"Map"	string
+		[28]	"."	string
+		[29]	"of"	string
+		[30]	"("	string
+		[31]	"1"	string
+		[32]	","	string
+		[33]	"2"	string
+		[34]	","	string
+		[35]	"3"	string
+		[36]	","	string
+		[37]	"4"	string
+		[38]	","	string
+		[39]	"5"	string
+		[40]	","	string
+		[41]	"6"	string
+		[42]	")"	string
+		[43]	";"	string
+		[44]	"map"	string
+		[45]	"."	string
+		[46]	"forEach"	string
+		[47]	"("	string
+		[48]	"("	string
+		[49]	"x"	string
+		[50]	","	string
+		[51]	"y"	string
+		[52]	")"	string
+		[53]	"->"	string
+		[54]	"LOGGER"	string
+		[55]	"."	string
+		[56]	"info"	string
+		[57]	"("	string
+		[58]	"x"	string
+		[59]	"y"	string
+		[60]	")"	string
+		[61]	")"	string
+		[62]	";"	string
+		[63]	"map"	string
+		[64]	"."	string
+		[65]	"forEach"	string
+		[66]	"("	string
+		[67]	"("	string
+		[68]	"Integer"	string
+		[69]	"x"	string
+		[70]	","	string
+		[71]	"Integer"	string
+		[72]	"y"	string
+		[73]	")"	string
+		[74]	"->"	string
+		[75]	"LOGGER"	string
+		[76]	"."	string
+		[77]	"info"	string
+		[78]	"("	string
+		[79]	"x"	string
+		[80]	"y"	string
+		[81]	")"	string
+		[82]	")"	string
+		[83]	";"	string
+		[84]	"map"	string
+		[85]	"."	string
+		[86]	"forEach"	string
+		[87]	"("	string
+		[88]	"("	string
+		[89]	"var"	string
+		[90]	"x"	string
+		[91]	","	string
+		[92]	"var"	string
+		[93]	"y"	string
+		[94]	")"	string
+		[95]	"->"	string
+		[96]	"LOGGER"	string
+		[97]	"."	string
+		[98]	"info"	string
+		[99]	"("	string
+		[100]	"x"	string
+		[101]	"y"	string
+		[102]	")"	string
+		[103]	")"	string
+		[104]	";"	string
+		[105]	"map"	string
+		[106]	"."	string
+		[107]	"forEach"	string
+		[108]	"("	string
+		[109]	"("	string
+		[110]	"@"	string
+		[111]	"NotNull"	string
+		[112]	"var"	string
+		[113]	"x"	string
+		[114]	","	string
+		[115]	"@"	string
+		[116]	"NotNull"	string
+		[117]	"var"	string
+		[118]	"y"	string
+		[119]	")"	string
+		[120]	"->"	string
+		[121]	"LOGGER"	string
+		[122]	"."	string
+		[123]	"info"	string
+		[124]	"("	string
+		[125]	"x"	string
+		[126]	"y"	string
+		[127]	")"	string
+		[128]	")"	string
+		[129]	";"	string
+		[130]	"Animal"	string
+		[131]	"object"	string
+		[132]	"null"	string
+		[133]	";"	string
+		[134]	"("	string
+		[135]	"object"	string
+		[136]	"instanceof"	string
+		[137]	"Dog"	string
+		[138]	")"	string
+		[139]	"{"	string
+		[140]	"Dog"	string
+		[141]	"d"	string
+		[142]	"("	string
+		[143]	"Dog"	string
+		[144]	")"	string
+		[145]	"object"	string
+		[146]	";"	string
+		[147]	"}"	string
+		[148]	"else"	string
+		[149]	"("	string
+		[150]	"object"	string
+		[151]	"instanceof"	string
+		[152]	"Cat"	string
+		[153]	")"	string
+		[154]	"{"	string
+		[155]	"Cat"	string
+		[156]	"kid"	string
+		[157]	"("	string
+		[158]	"Cat"	string
+		[159]	")"	string
+		[160]	"object"	string
+		[161]	";"	string
+		[162]	"}"	string
+		[163]	"System"	string
+		[164]	"."	string
+		[165]	"out"	string
+		[166]	"."	string
+		[167]	"println"	string
+		[168]	"("	string
+		[169]	"object"	string
+		[170]	"."	string
+		[171]	"name"	string
+		[172]	"("	string
+		[173]	")"	string
+		[174]	")"	string
+		[175]	";"	string
+		[176]	"}"	string
+		[177]	"}"	string
++		Raw View		
+-		file.Operators	Count = 12	System.Collections.Generic.List<string>
+		[0]	"<"	string
+		[1]	">"	string
+		[2]	"="	string
+		[3]	"+"	string
+		[4]	"+"	string
+		[5]	"+"	string
+		[6]	"+"	string
+		[7]	"="	string
+		[8]	"if"	string
+		[9]	"="	string
+		[10]	"if"	string
+		[11]	"="	string
++		Raw View		
++		file.Comments	Count = 2	(without counting this first block of comments)
+
+		BM_CognitiveComplexity	5	double
+		BM_CouplingBetweenObjectClasses	0	double
+		BM_CyclomaticComplexity	6	double
+		BM_DepthOfInheritance	0	int
+		BM_EffectiveComplexityScoreLogicalComplexity	0	double
+		BM_EffectiveComplexityScoreRelations	0	double
+		BM_EffectiveComplexityScoreSoftwareScience	0	double
+		BM_HalsteadNumOperands	166	double
+		BM_HalsteadNumOperators	12	double
+		BM_HalsteadNumUniqueOperands	47	double
+		BM_HalsteadNumUniqueOperators	5	double
+		BM_InherentComplexityScoreLogicalComplexity	0	double
+		BM_InherentComplexityScoreRelations	0	double
+		BM_InherentComplexityScoreSoftwareScience	0	double
+		BM_LackOfCohesion	1	double
+		BM_LinesOfCode	16	int
+		BM_LinesOfComments	2	int
+		BM_LinesPseudoDeleted	0	int
+		BM_NestingDepth	2	int
+		BM_Size	518	long
+
+
+ */
+import java.util.Map;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// Var in Lambdas
+		Map<Integer, Integer> map = Map.of(1, 2, 3, 4, 5, 6);
+		map.forEach((x, y) -> LOGGER.info(x + y));
+		map.forEach((Integer x, Integer y) -> LOGGER.info(x + y));
+		map.forEach((var x, var y) -> LOGGER.info(x + y));
+		map.forEach((@NotNull var x, @NotNull var y) -> LOGGER.info(x + y));
+
+		Animal object = null;
+		// instanceof
+		if (object instanceof Dog) {
+			Dog d = (Dog) object;
+		} else if (object instanceof Cat) {
+			Cat kid = (Cat) object;			
+		}
+		System.out.println(object.name());
+	}
+
+}
